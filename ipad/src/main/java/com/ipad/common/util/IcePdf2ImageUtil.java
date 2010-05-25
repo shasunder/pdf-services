699adb2ea70d6a8f4e7801c02e21042d97b720d3
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2010-05-25 20:30:33 +0000

    Ice pdf

---
+++ ipad/src/main/java/com/ipad/common/util/IcePdf2ImageUtil.java
@@ -0,0 +1,64 @@
+package com.ipad.common.util;
+
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.icepdf.core.pobjects.Document;
+import org.icepdf.core.pobjects.Page;
+import org.icepdf.core.util.GraphicsRenderingHints;
+
+public class IcePdf2ImageUtil {
+
+	/**
+	 * Extract image from PDF Document
+	 */
+	public List<BufferedImage> pdfToImage(InputStream in) {
+
+		logger.debug("Parsing image from PDF file ....");
+		List<BufferedImage> result = new ArrayList<BufferedImage>();
+
+		Document document = new Document();
+		try {
+			document.setInputStream(in, "icepdf-sample.pdf");
+
+			float scale = 1.0f;
+			float rotation = 0f;
+
+			// Paint each pages content to an image and write the image to file
+			for (int i = 0; i < document.getNumberOfPages(); i++) {
+				BufferedImage image = (BufferedImage) document.getPageImage(i, GraphicsRenderingHints.SCREEN, Page.BOUNDARY_CROPBOX, rotation, scale);
+				logger.debug("\t capturing page " + i);
+				result.add(image);
+				image.flush();
+			}
+
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		} finally {
+			document.dispose();
+		}
+
+		return result;
+	}
+
+	private IcePdf2ImageUtil() {
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+
+	public static synchronized IcePdf2ImageUtil getInstance() {
+		return _instance;
+	}
+
+	private static Log logger = LogFactory.getLog(IcePdf2ImageUtil.class.getName());
+	private static IcePdf2ImageUtil _instance = new IcePdf2ImageUtil();
+
+}
---
+++ ipad/src/main/java/com/ipad/web/IcePdf2ImageController.java
@@ -0,0 +1,112 @@
+/*
+ * @author Sandeep.Maloth
+ */
+package com.ipad.web;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ipad.common.util.IcePdf2ImageUtil;
+import com.ipad.common.util.Pdf2ImageUtil;
+import com.ipad.service.PdfService;
+
+/**
+ * Controller to handle Pdf CRUD requests.
+ * <p>
+ * Delegates incoming requests to Pdf service
+ * @see com.ipad.service.PdfService
+ * <p>
+ * Exceptions are trapped by spring managed exception resolver.
+ * @see com.ipad.web.ExceptionResolver
+ * <p>
+ * http://www.icepdf.org/
+ * 
+ * 
+ */
+@Controller(value = "icePdfController")
+public class IcePdf2ImageController {
+
+	private static Log logger = LogFactory.getLog(IcePdf2ImageController.class);
+	final WebHelper webHelper = WebHelper.getInstance();
+
+	PdfService pdfService;
+
+	@RequestMapping("/ice/pdfById.do")
+	public ModelAndView getById(HttpServletResponse response) {
+		return new ModelAndView("pdf2image/ice-upload-view");
+	}
+
+	@RequestMapping("/ice/pdfCreate.do")
+	public void post(@ModelAttribute("fileUploadForm")
+	FileUploadForm fileUploadForm, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		CommonsMultipartFile file = fileUploadForm.getFile();
+
+		InputStream inputStream = file.getInputStream();
+		try {
+			List<BufferedImage> pdfToImages = IcePdf2ImageUtil.getInstance().pdfToImage(inputStream);
+
+			logger.info("Received : " + file.getOriginalFilename());
+
+			response.setContentType("image/png");
+			BufferedImage image = Pdf2ImageUtil.joinImages(pdfToImages);
+			Pdf2ImageUtil.write(image, "png", response.getOutputStream());
+			response.flushBuffer();
+		} finally {
+			inputStream.close();
+		}
+	}
+
+	@RequestMapping("/ice/pdfURL.do")
+	public void getUrl(@RequestParam("url")
+	String url, @ModelAttribute("fileUploadForm")
+	FileUploadForm fileUploadForm, HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		URL urlObj = new URL(url);
+		InputStream in = urlObj.openStream();
+		try {
+			List<BufferedImage> pdfToImages = IcePdf2ImageUtil.getInstance().pdfToImage(in);
+			logger.info("Received : " + url);
+
+			response.setContentType("image/png");
+			BufferedImage image = Pdf2ImageUtil.joinImages(pdfToImages);
+			Pdf2ImageUtil.write(image, "png", response.getOutputStream());
+			response.flushBuffer();
+		} finally {
+			in.close();
+		}
+	}
+
+	@RequestMapping("/ice/pdfUpdate.do")
+	public ModelAndView put(@RequestParam("pdfId")
+	String pdfId, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		return null;
+	}
+
+	@RequestMapping("/ice/pdfDelete.do")
+	public ModelAndView delete(@RequestParam("pdfId")
+	String pdfId, HttpServletResponse response) {
+		return null;
+	}
+
+	@Autowired
+	public void setPdfService(PdfService pdfService) {
+		this.pdfService = pdfService;
+	}
+
+}
