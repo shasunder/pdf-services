195d94e39947a23d66fd19088b181d3554c02851
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2010-12-15 22:00:41 +0000

    better color scheme

--- php/matrimony/templates/homefooter.template.php
+++ php/matrimony/templates/homefooter.template.php
@@ -15,21 +15,21 @@
 </div>
 	<div class="page">
 		<div class="fr_fullpage">
-			<div class="fr_top_content"> <a href="contactus.php" style="text-decoration:none; color:black;"> Contact us </a> |  <a  href="services.php"style="text-decoration:none; color:black;"> Matrimonial Services </a> |  <a href="aboutus.php" style="text-decoration:none; color:black;"> About us </a>  |  <a style="text-decoration:none; color:black" href="javascript:void(0)" onclick = "document.getElementById('fade').style.display='block';showmain('4');"> Tour </a> | <a href="advertise.php" style="text-decoration:none; color:black;"> Advertise with Us </a> |  <a href="privacy.php" style="text-decoration:none; color:black;"> Privacy Policy </a> | <a href="sitemap.php" style="text-decoration:none; color:black;"> Sit Map </a> |  Live Chat</div> 
+			<div class="fr_top_content"> <a href="contactus.php" style="text-decoration:none; color:black;"> Contact us </a> |  <a  href="services.php"style="text-decoration:none; color:black;"> Matrimonial Services </a> |  <a href="aboutus.php" style="text-decoration:none; color:black;"> About us </a>  |  <a style="text-decoration:none; color:black" href="javascript:void(0)" onclick = "document.getElementById('fade').style.display='block';showmain('4');"> Tour </a> | <a href="advertise.php" style="text-decoration:none; color:black;"> Advertise with Us </a> |  <a href="privacy.php" style="text-decoration:none; color:black;"> Privacy Policy </a> | <a href="sitemap.php" style="text-decoration:none; color:black;"> Sit Map </a> |  Live Chat</div>
 		<div>
-			<div class="fr_bottom_pg" > 
+			<div class="fr_bottom_pg" >
 				<table width="770" border="0" cellspacing="0" cellpadding="0">
 				<tr>
 				<td width="6"><img src="images/fr_left.gif" width="6" height="26" /></td>
-				<td width="760" style="background-image:url(images/fr_center.gif); background-repeat:repeat-x;"> 
-				<div class="poweredlink"> <label> Powered by Benivolent Soft Tech</label> Copyright (c)  2008. All Rights Reserved </div>
-				
+				<td width="760" style="background-image:url(images/fr_center.gif); background-repeat:repeat-x;">
+				<div class="poweredlink"> <label> Powered by Tucantis Services Ltd</label> Copyright (c)  2009. All Rights Reserved </div>
+
 				</td>
 				<td width="6"><img src="images/fr_right.gif" width="6" height="26" /></td>
-				
+
 				</tr>
 				</table>
-			</div> 
+			</div>
 		</div>
 	</div>
 </div>
