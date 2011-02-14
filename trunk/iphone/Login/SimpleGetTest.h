3091405a74450d4ff38c8a30a762430f1a391506
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-02-14 01:00:57 +0000

    added

---
+++ trunk/iphone/Login/SimpleGetTest.h
@@ -0,0 +1,19 @@
+//
+//  SimpleGetTest.h
+//  JavaIQ
+//
+//  Created by sandeep m on 08/09/2010.
+//  Copyright 2010 bri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+
+@interface SimpleGetTest : SenTestCase {
+
+}
+
+@end
