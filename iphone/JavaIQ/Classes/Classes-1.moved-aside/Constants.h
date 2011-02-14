6a7a4313648b3f09c5e751d77d5599ecfe250641
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-02-14 01:37:31 +0000

    added

---
+++ iphone/JavaIQ/Classes/Classes-1.moved-aside/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  JavaIQ
+//
+//  Created by sandeep m on 16/09/2010.
+//  Copyright 2010 bri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Constants : NSObject {
+
+}
+
+@end
