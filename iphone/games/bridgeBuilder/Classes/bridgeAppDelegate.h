0ea5ca8f57e562b02ce514bd54ce199374d37b0f
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-02-27 00:34:12 +0000

    using cocos 2d box2d

---
+++ iphone/games/bridgeBuilder/Classes/bridgeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  bridgeAppDelegate.h
+//  bridge
+//
+//  Created by sandeep m on 27/02/2011.
+//  Copyright bri 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bridgeAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
