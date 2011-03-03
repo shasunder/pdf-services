b1a34f96afb4c78362c2a15ab1592f30e68fe472
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-03-03 21:58:14 +0000

    added tilemap and touch action

---
+++ trunk/iphone/games/bridge/Classes/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  bridge
+//
+//  Created by sandeep m on 28/02/2011.
+//  Copyright 2011 bri. All rights reserved.
+//
+
+#import "Constants.h"
+
+
+@implementation Constants
+
+#defint DEFAULT_BACKGROUND @"wood2.jpg"
+@end
