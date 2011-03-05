b29374cb2df30c3fa6e55f261bd43c8b325f701f
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-03-05 20:45:55 +0000

    util

---
+++ trunk/iphone/games/bridge/Classes/CocosUtility.h
@@ -0,0 +1,19 @@
+//
+//  CocosUtility.h
+//  bridge
+//
+//  Created by sandeep m on 05/03/2011.
+//  Copyright 2011 bri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CocosUtility : CCSprite {
+
+}
+
+- (CGRect)getRectangle:(CCSprite *) sprite;
+- (BOOL)containsTouchLocation:(UITouch *)touch : (CCSprite *) sprite;
+
+@end
