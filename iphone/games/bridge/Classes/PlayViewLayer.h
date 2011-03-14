11a23ac4d92a029128d8db17e2754bd80c8536d2
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-03-14 21:25:50 +0000

    integrated box2d sample

--- iphone/games/bridge/Classes/PlayViewLayer.h
+++ iphone/games/bridge/Classes/PlayViewLayer.h
@@ -11,12 +11,20 @@
 #import "CocosUtility.h"
 #import "Bridge.h"
 #import "PlayScene.h"
+#import "Constants.h"
+#import "Box2D.h"
+#import "GLES-Render.h"
 
+#define PTM_RATIO 32.0
 @interface PlayViewLayer : CCLayer {
 	Bridge *bridge;
 	
 	CCRenderTexture* target;
 	CCSprite* brush;
+	
+	b2World* _world;
+	b2Body* _body;
+	CCSprite* _ball;
 }
 
 @end
