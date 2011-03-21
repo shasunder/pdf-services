387a810575b5d77d4c1e5ccca9c226b6cf4f34b1
sandeep.maloth <sandeep.maloth@c278b1f6-0900-11df-a26f-8b716316a5bc>
2011-03-21 20:39:13 +0000

    main

---
+++ trunk/iphone/games/bridge/Classes/BridgeMain.h
@@ -0,0 +1,21 @@
+//
+//  BridgeMain.h
+//  bridge
+//
+//  Created by sandeep m on 21/03/2011.
+//  Copyright 2011 bri. All rights reserved.
+//
+#import "cocos2d.h"
+#import "EditorScene.h"
+#import "PlayScene.h"
+#import "Constants.h"
+#import "Bridge.h"
+#import "BridgeContext.h"
+
+@interface BridgeMain : NSObject {
+
+}
+
+-(void) startLevel:(int) level;
+-(void) start;
+@end
